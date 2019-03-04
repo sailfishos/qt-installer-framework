4a3c16f2c4eea43665c7a853f20214060b8d8bf4
Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>
2019-03-04 11:52:32 +0200

    Arrange LicenseAgreementPage licenses and filter duplicates
    - Add a "priority" attribute to the "License" element used in "package.xml"
      for arranging licenses by priority in the LicenseAgreementPage. Priority >0
      shows the license on top of others.
    - Arrange the licenses with the same priority alphabetically
    - Filter duplicate licenses from the LicenseAgreementPage
    
    Change-Id: I1dcacdd417d2383b8dc18149e4de329fbf11cfe8

--- src/libs/installer/packagemanagergui.h
+++ src/libs/installer/packagemanagergui.h
@@ -279,7 +279,8 @@ private Q_SLOTS:
     void currentItemChanged(QListWidgetItem *current);
 
 private:
-    void addLicenseItem(const QHash<QString, QPair<QString, QString> > &hash);
+    void addLicenseItem(const QHash<QString, QVariantMap> &hash);
+    void createLicenseWidgets();
     void updateUi();
 
 private:
@@ -291,6 +292,8 @@ private:
 
     QLabel *m_acceptLabel;
     QLabel *m_rejectLabel;
+
+    QHash<QString, QVariantMap> m_licenseItems;
 };
 
 
